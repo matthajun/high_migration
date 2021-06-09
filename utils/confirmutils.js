c0fa60767fbaaf4fdf2139b5acaadac515c2014e
ctilab <matthajun@ctilab.co.kr>
2021-06-09 13:15:09 +0900

    first

--- utils/confirmutils.js
+++
@@ -1,9 +0,0 @@
-const CRC32 = require('crc-32');
-const fs = require('fs');
-
-module.exports.makeConfirmCode  = function (strData){
-    let a = CRC32.str('"body":' + JSON.stringify(strData));
-    let b = a>>>0;
-
-    return b.toString(16);
-};
